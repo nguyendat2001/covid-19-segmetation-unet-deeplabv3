34abbc8f04d3c60f738d60ddf80c068a26775f2e
UNIT\datnt1 <datnt1@unit.com.vn>
2024-06-12 16:29:28 +0700

    update source code

--- trainval.py
+++ trainval.py
@@ -78,8 +78,8 @@ def get_training_augmentation():
         albu.PadIfNeeded(min_height=512, min_width=512, always_apply=True, border_mode=0),
         albu.RandomCrop(height=512, width=512, always_apply=True),
 
-        # albu.IAAAdditiveGaussianNoise(p=0.2),
-        albu.Perspective(p=0.5),
+        albu.IAAAdditiveGaussianNoise(p=0.2),
+        albu.IAAPerspective(p=0.5),
 
 #         albu.OneOf(
 #             [
@@ -92,7 +92,7 @@ def get_training_augmentation():
 
         albu.OneOf(
             [
-                albu.IAASharpen(p=1),
+                albu.Sharpen(p=1),
                 albu.Blur(blur_limit=3, p=1),
                 albu.MotionBlur(blur_limit=3, p=1),
             ],
